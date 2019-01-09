84f0e48ee233eb9661176a15510ffb0528ce3f23
Vernieri <jvmendes.ti@gmail.com>
2019-01-09 16:04:06 -0200

    Create smokeTest.java

---
+++ simple-bots/cucumber/smokeTest.java
@@ -0,0 +1,14 @@
+package stepDefinition;
+
+
+
+public class SmokeTest {
+
+	@Given("^Open firefox and start application$")
+	public void Open_firefox_and_start_application() throws Throwable {
+		throw new PendingException();
+	}
+	
+
+	
+}
