3055b4ae089c1a7eb486cd4bc47f9f469e8129e1
Vernieri <jvmendes.ti@gmail.com>
2019-01-09 16:41:20 -0200

    Update TestRunner.java

--- simple-bots/cucumber/TestRunner.java
+++ simple-bots/cucumber/TestRunner.java
@@ -7,7 +7,8 @@ import cucumber.api.junit.Cucumber;
 @Cucumber.Options(
 		features = "features",
 		glue = {"stepDefination"},
-		plugin ={"html:target/cucumber-html-report"}
+		plugin ={"html:target/cucumber-html-report", "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
+				 "usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml"}
 		)
 public class TestRunner {
 	
