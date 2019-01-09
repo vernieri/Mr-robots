9e7a0bcb137612832c4d94ddfb56c846aa6cb6c2
Vernieri <jvmendes.ti@gmail.com>
2019-01-09 16:04:26 -0200

    Update smokeTest.java

--- simple-bots/cucumber/smokeTest.java
+++ simple-bots/cucumber/smokeTest.java
@@ -9,6 +9,10 @@ public class SmokeTest {
 		throw new PendingException();
 	}
 	
+	@When("^I enter valid username and valid password$")
+	public void I_enter_valid_username_and_valid_password() throws Throwable{
+		throw new PendingException();
+	}	
 
 	
 }
