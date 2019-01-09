1752d687c51184111d4abd0cd06155370f440512
Vernieri <jvmendes.ti@gmail.com>
2019-01-09 16:04:57 -0200

    Update smokeTest.java

--- simple-bots/cucumber/smokeTest.java
+++ simple-bots/cucumber/smokeTest.java
@@ -1,6 +1,9 @@
 package stepDefinition;
 
-
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class SmokeTest {
 
