0ea409b4d80f52d47e3953ea24dbf6c861dc3837
Vernieri <jvmendes.ti@gmail.com>
2019-03-19 22:19:30 -0300

    Update VerifyLinks.java

--- simple-bots/VerifyLinks.java
+++ simple-bots/VerifyLinks.java
@@ -2,7 +2,6 @@ package links;  //Comenta aqui pq esse é o pacote
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
- 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
